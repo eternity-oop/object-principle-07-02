e8498cd1974ee11e3fe88f0771020388411652b6
조영호 <aeternum9342@gmail.com>
2025-05-07 22:42:19 +0900

    오브젝트 설계 원칙편 7-2 예제

---
+++ src/test/java/org/eternity/call/CallCollectorTest.java
@@ -0,0 +1,17 @@
+package org.eternity.call;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CallCollectorTest {
+    @Test
+    public void collect() {
+        CallCollector callCollector = new CallCollector(new JsonReader("calls.json"));
+        CallHistory history = callCollector.collect("010-1111-2222");
+
+        assertThat(history.callDuration()).isEqualTo(Duration.ofSeconds(159));
+    }
+}
